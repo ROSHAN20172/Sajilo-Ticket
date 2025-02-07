8c88aab8f7c922cb3e1859e0166c731814b98755
ROSHAN20172 <np03cs4a220020@heraldcollege.edu.np>
2025-02-07 16:07:13 +0545

    Bus Animation added at homepage

--- Frontend/sajilo-ticket/src/pages/home/hero/Hero.jsx
+++ Frontend/sajilo-ticket/src/pages/home/hero/Hero.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
 import { motion } from 'framer-motion'
-import Search from './search/Search';
-import RootLayout from '../../../layout/RootLayout';
+import Search from './search/Search'
+import RootLayout from '../../../layout/RootLayout'
+
+import '../../../css/BusAnimator.css';
+import busImage from '../../../assets/Bus Animator/bus.png';
+import wheelImage from '../../../assets/Bus Animator/wheel.png';
+import roadImage from '../../../assets/Bus Animator/road.jpg';
+import skyImage from '../../../assets/Bus Animator/sky.jpg';
+import cityImage from '../../../assets/Bus Animator/city.png';
+
 
 const Hero = () => {
 
@@ -13,16 +20,32 @@ const Hero = () => {
 
     return (
         <motion.div
-            className='w-full flex-1 h-screen bg-[url("./assets/herobg.png")] bg-cover bg-no-repeat bg-top relative'
+            className='w-full flex-1 h-screen relative'
             initial="hidden"
             animate="visible"
             exit="hidden"
             variants={variants}
             transition={{ duration: 0.85, ease: "easeInOut" }}
         >
-            
-            <RootLayout className="absolute top-0 left-0 w-full h-full py-[9ch] bg-gradient-to-b from-neutral-50/70 via-neutral-50/15 to-neutral-50/5 flex items-center justify-start text-center flex-col gap-9">
+            <div className="hero" style={{ backgroundImage: `url(${skyImage})` }}>
 
+                {/* Highway */}
+                <div className="highway" style={{backgroundImage: `url(${roadImage})`}}></div>
+
+                {/* City */}
+                <div className="city" style={{backgroundImage: `url(${cityImage})`}}></div>
+
+                {/* Bus and Wheels */}
+                <div className="bus">
+                    <img src={busImage} alt="Bus" />
+                    <div className="wheel">
+                        <img src={wheelImage} className="frontwheel" alt="Front Wheel" />
+                        <img src={wheelImage} className="backwheel" alt="Back Wheel" />
+                    </div>
+                </div>
+            </div>
+
+            <RootLayout className="absolute top-0 left-0 w-full h-full py-[9ch] bg-gradient-to-b from-neutral-50/70 via-neutral-50/15 to-neutral-50/5 flex items-center justify-start text-center flex-col gap-9">
                 {/* Title Section */}
                 <div className="space-y-2">
                     <motion.p
@@ -32,7 +55,7 @@ const Hero = () => {
                         transition={{ duration: 2, ease: "easeOut" }}
                         className="text-lg text-neutral-500 font-medium"
                     >
-                        Get you bus tickets
+                        Sajilo Ticket – Easy Online Bus Ticket Booking
                     </motion.p>
 
                     <motion.h1
@@ -42,17 +65,16 @@ const Hero = () => {
                         transition={{ duration: 1.85, ease: "easeOut" }}
                         className="text-5xl text-neutral-800 font-bold capitalize"
                     >
-                        find best bus for you!
+                        Book Your Next Journey With Us
                     </motion.h1>
                 </div>
 
                 {/* Search Section */}
                 <Search />
-
             </RootLayout>
-
-        </motion.div >
+        </motion.div>
     )
 }
 
 export default Hero
+
